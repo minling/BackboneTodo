a8532713dbbb04ff12fd4c263ca221d3f4e9df79
minling <mzhao725@gmail.com>
2015-10-10 22:33:21 -0400

    completes adding to list function for todo list

---
+++ scripts.js
@@ -0,0 +1,57 @@
+var Todo = Backbone.Model.extend({
+  defaults: {
+    task: '',
+    priority: ''
+  }
+});
+
+var Todos = Backbone.Collection.extend({});
+
+var todos = new Todos();
+
+//Backbone view for one todo
+var TodoView = Backbone.View.extend({
+  model: new Todo(),
+  tagName: 'tr',
+  initialize: function() {
+    this.template = _.template($('.todos-list-template').html());
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
+
+//Backbone view for all todos
+var TodosView = Backbone.View.extend({
+  model: todos,
+  el: $('.todos-list'), 
+  initialize: function() {
+    this.model.on('add', this.render, this);
+  },
+  render: function() {
+    var self = this;
+    this.$el.html('');
+    _.each(this.model.toArray(), function(todo) {
+      self.$el.append((new TodoView({model: todo})).render().$el);
+    });
+    return this;
+  }
+});
+
+var todosView = new TodosView();
+
+$(document).ready(function() {
+  $('.add-todo').on('click', function() {
+    var todo = new Todo({
+      task: $('.task-input').val(),
+      priority: $('.priority-input').val()
+    });
+    $('.task-input').val('');
+    $('.priority-input').val('');
+    todos.add(todo);
+  });
+});
+
+
+
